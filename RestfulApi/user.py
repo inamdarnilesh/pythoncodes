5944d48b2a6b13a00f9923c37adeef4598de4caf
Nilesh Inamdar <NILESHI@amdocs.com>
2019-09-25 18:10:28 +0530

    Testing git

---
+++ RestfulApi/user.py
@@ -0,0 +1,7 @@
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+# -*- coding: utf-8 -*-
+
